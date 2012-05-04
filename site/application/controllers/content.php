184ebd389821a65596d42895343cb14ccb491ef8
Nick Jackson <nick@nickjackson.me>
2012-05-04 11:08:05 +0100

    Caching is a dick

--- site/application/controllers/content.php
+++ site/application/controllers/content.php
@@ -7,8 +7,6 @@ class Content extends CI_Controller {
 	
 		$head['page'] = 'home';
 	
-		$this->output->cache(30);
-	
 		$this->load->view('inc/head', $head);
 		$this->load->view('home');
 		$this->load->view('inc/foot');
@@ -21,8 +19,6 @@ class Content extends CI_Controller {
 		
 		$this->load->model('activity_model');
 		$data['verbs'] = $this->activity_model->get_verbs();
-	
-		$this->output->cache(5);
 	
 		$this->load->view('inc/head', $head);
 		$this->load->view('docs', $data);
